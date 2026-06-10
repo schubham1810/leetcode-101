40536603774f095a565709bd0aecb51ae72c0de6
Shubham Saxena <shubhamanandalaya@gmail.com>
2026-06-10 23:37:53 +0530

    0219_contains_duplicate_ii.java

---
+++ Completed/0219_contains_duplicate_ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                // System.out.println(Math.abs(map.get(nums[i])-i));
+                if(Math.abs(map.get(nums[i])-i)<=k) return true;
+                else map.put(nums[i],i);
+            }else{
+                map.put(nums[i],i);
+            }
+        }
+        return false;
+    }
+}

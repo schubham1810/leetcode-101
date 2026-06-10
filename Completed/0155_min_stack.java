c7e6645f8bac72334f1ce06aed063e4022ad7365
Shubham Saxena <shubhamanandalaya@gmail.com>
2026-06-10 23:37:12 +0530

    0155_min_stack.java

---
+++ Completed/0155_min_stack.java
@@ -0,0 +1,38 @@
+class MinStack {
+
+    Stack<Integer> st;
+    int min;
+    public MinStack() {
+        st = new Stack<>();
+        min = Integer.MAX_VALUE;
+    }
+    
+    public void push(int x) {
+        if(x <= min){          
+            st.push(min);
+            min=x;
+        }
+        st.push(x);
+    }
+    
+    public void pop() {
+        if(st.pop() == min) min=st.pop();        
+    }
+    
+    public int top() {
+        return st.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */

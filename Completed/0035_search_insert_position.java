afb76fa9148aa9acc82c37fc603fdc3743bc608f
Shubham Saxena <shubhamanandalaya@gmail.com>
2026-06-10 23:34:01 +0530

    0035_search_insert_position.java

---
+++ Completed/0035_search_insert_position.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+         int low=0;
+        int high=nums.length-1;
+        int index=-1;
+        while(low<=high){
+            int mid= (low+high)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                index=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return index+1;
+    }
+}

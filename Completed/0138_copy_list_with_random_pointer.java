dfffec516bb9ddd42083a1c5797b4f32d81b53ca
Shubham Saxena <shubhamanandalaya@gmail.com>
2026-06-10 23:36:40 +0530

    0138_copy_list_with_random_pointer.java

---
+++ Completed/0138_copy_list_with_random_pointer.java
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        Node temp= head;
+        while(temp!=null){
+            Node newNode = new Node(temp.val);
+            newNode.next = temp.next;
+            temp.next = newNode;
+            temp = temp.next.next;
+        }
+        temp = head;
+        while(temp!=null){
+            if(temp.random==null){
+                temp.next.random = null;
+            }else{
+                temp.next.random = temp.random.next;
+            }
+            temp = temp.next.next;
+        }
+        temp = head;
+        Node dummy = new Node(-1);
+        Node res = dummy;
+        while(temp!=null){
+            Node newNode = temp.next;
+            temp.next = temp.next.next;
+            newNode.next = null;
+            res.next = newNode;
+            res = res.next;
+            temp = temp.next;
+        }
+        return dummy.next;
+    }
+}

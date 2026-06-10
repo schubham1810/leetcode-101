f65e2680779a8794fccd90d88120eda159d36131
Shubham Saxena <shubhamanandalaya@gmail.com>
2026-06-10 23:38:45 +0530

    0242_valid_anagram.java

---
+++ Completed/0242_valid_anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s == null || t == null) return false;
+        if(s.length() != t.length()) return false;
+        int[] counter = new int[26];
+        for(int idx = 0; idx < s.length(); idx++){
+            counter[s.charAt(idx)-'a']++;
+            counter[t.charAt(idx)-'a']--;
+        }
+
+        for(int idx: counter){
+            if(idx != 0)
+                return false;
+        }
+        return true;
+    }
+}

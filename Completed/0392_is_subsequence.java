135d2f4b0c278be4e6a53510ecbd208229591805
Shubham Saxena <shubhamanandalaya@gmail.com>
2026-06-10 23:39:33 +0530

    0392_is_subsequence.java

---
+++ Completed/0392_is_subsequence.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.equals("") ) return true;
+        int j=0;
+        for(int i=0;i<t.length();i++){
+            if(j==s.length()) break;
+            if(s.charAt(j)==t.charAt(i)){
+                j++;
+            }
+        }
+        return j==s.length();
+    }
+}
